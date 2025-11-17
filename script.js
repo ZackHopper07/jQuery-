45229f47947f33dcdce3f5d74ae7418147e64274
Vinit Shrestha <shresthavinit@gmail.com>
2025-11-17 10:14:51 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
             $("#showContentBtn").click(function() {
-                $("#extraContent").slideToggle(); // Toggle content with slide animation
+                $("#extraContent").slideToggle(); 
                 $(this).text(function(i, text){
                     return text === "Show More" ? "Show Less" : "Show More";
                 });
+            });
+
+            $("#hamburger").on('click', function() {
+                var expanded = $(this).attr('aria-expanded') === 'true';
+                $(this).attr('aria-expanded', (!expanded).toString());
+                $('#nav').toggleClass('open');
+                $(this).toggleClass('is-active');
+            });
+
+            $("#nav a").on('click', function() {
+                $('#nav').removeClass('open');
+                $('#hamburger').attr('aria-expanded','false').removeClass('is-active');
             });
         });
